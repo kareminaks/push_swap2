c4cc9018b4cd9b5b9d38a6f0207ca6a1308300c0
Ksenia Karemina <kseniakaremina@mbp-ksenia.speedport.ip>
2023-06-19 13:01:54 +0200

    xfh

--- instructions.c
+++ instructions.c
@@ -3,12 +3,13 @@
 //sa (swap a): Swap the first 2 elements at the top of stack a.
 //Do nothing if there is only one or no elements.
 
-void sa(list_t* list_a, int print)
+void sa(list_t* list_a, int print,list_t *command_list)
 {
     // printf("a before:\n");
     // print_list(list_a);
     if (print)
-    printf("sa\n");
+    //printf("sa\n");
+    put_command_in_list(SA,command_list);
 
     if ((list_a->head == list_a->tail) || (!list_a->head)) {
         return;
@@ -23,12 +24,13 @@ void sa(list_t* list_a, int print)
 }
 //sb (swap b): Swap the first 2 elements at the top of stack b.
 //Do nothing if there is only one or no elements.
-void sb(list_t* list_b, int print)
+void sb(list_t* list_b, int print,list_t *command_list)
 {
     // printf("b before:\n");
     // print_list(list_b);
     if (print)
-    printf("sb\n");
+    //printf("sb\n");
+    put_command_in_list(SB,command_list);
 
     if ((list_b->head == list_b->tail) || (!list_b->head)) {
         return;
@@ -40,12 +42,13 @@ void sb(list_t* list_b, int print)
 }
 
 //ss : sa and sb at the same time.
-void ss(list_t* list_a,list_t* list_b, int print)
+void ss(list_t* list_a,list_t* list_b, int print,list_t *command_list)
 {
     if (print)
-    printf("ss\n");
-    sa(list_a, 0);
-    sb(list_b, 0);
+    //printf("ss\n");
+    put_command_in_list(SS,command_list);
+    sa(list_a, 0,command_list);
+    sb(list_b, 0,command_list);
 }
 
 node_t* pop_head(list_t* list) {
@@ -118,14 +121,15 @@ void push_tail(list_t* list, node_t* node)
 }
 //pa (push a): Take the first element at the top of b and put it at the top of a.
 //Do nothing if b is empty.
-void pa(list_t* list_a, list_t* list_b, int print)
+void pa(list_t* list_a, list_t* list_b, int print,list_t *command_list)
 {
     // printf("a before:\n");
     // print_list(list_a);
     // printf("b before:\n");
     // print_list(list_b);
     if (print) 
-        printf("pa\n");
+        //printf("pa\n");
+        put_command_in_list(PA,command_list);
     node_t* head = pop_head(list_b);
     if (!head) return;
     push_head(list_a, head);
@@ -139,14 +143,15 @@ void pa(list_t* list_a, list_t* list_b, int print)
 //pb (push b): Take the first element at the top of a and put it at the top of b.
 //Do nothing if a is empty.
 
-void pb(list_t* list_b, list_t* list_a, int print)
+void pb(list_t* list_b, list_t* list_a, int print,list_t *command_list)
 {
     // printf("a before:\n");
     // print_list(list_a);
     // printf("b before:\n");
     // print_list(list_b);
     if (print)
-    printf("pb\n");
+    //printf("pb\n");
+        put_command_in_list(PB,command_list);
     node_t* head = pop_head(list_a);
     if (!head) return;
     push_head(list_b, head);
@@ -158,40 +163,42 @@ void pb(list_t* list_b, list_t* list_a, int print)
 
 //ra (rotate a): Shift up all elements of stack a by 1.
 //The first element becomes the last one.
-void ra(list_t* list_a, int print)
+void ra(list_t* list_a, int print,list_t *command_list)
 {
     // printf("a before:\n");
     // print_list(list_a);
     if (print)
-    printf("ra\n");
+    //printf("ra\n");
+        put_command_in_list(RA,command_list);
     node_t* head = pop_head(list_a);
     if (!head) return;
     push_tail(list_a, head);
 }
 //rb (rotate b): Shift up all elements of stack b by 1.
 //The first element becomes the last one.
-void rb(list_t* list_b, int print)
+void rb(list_t* list_b, int print,list_t *command_list)
 {
     // printf("b before:\n");
     // print_list(list_b);
     if (print)
-    printf("rb\n");
+    //printf("rb\n");
+        put_command_in_list(RB,command_list);
     node_t* head = pop_head(list_b);
     if (!head) return;
     push_tail(list_b, head);
 }
 //rr : ra and rb at the same time.
-void rr(list_t* list_a, list_t *list_b, int print){
+void rr(list_t* list_a, list_t *list_b, int print,list_t *command_list){
     if (print)
-    printf("rr\n");
-
-    ra(list_a, 0);
-    rb(list_b, 0);
+   // printf("rr\n");
+    put_command_in_list(RR,command_list);
+    ra(list_a, 0,command_list);
+    rb(list_b, 0,command_list);
 }
 
 //rra (reverse rotate a): Shift down all elements of stack a by 1.
 //The last element becomes the first one.
-void rra(list_t* list_a, int print){
+void rra(list_t* list_a, int print,list_t *command_list){
     // printf("a before:\n");
     // print_list(list_a);
 
@@ -199,7 +206,8 @@ void rra(list_t* list_a, int print){
     //     list_a->head->next, list_a->head->prev, list_a->tail->next, list_a->tail->prev);
 
     if (print)
-    printf("rra\n");
+    //printf("rra\n");
+        put_command_in_list(RRA,command_list);
     node_t* tail = pop_tail(list_a);
     if(!tail) return;
     push_head(list_a, tail);
@@ -207,20 +215,21 @@ void rra(list_t* list_a, int print){
 
 //rrb (reverse rotate b): Shift down all elements of stack b by 1.
 //The last element becomes the first one.
-void rrb(list_t* list_b, int print){
+void rrb(list_t* list_b, int print,list_t *command_list){
     // printf("b before:\n");
     // print_list(list_b);
     if (print)
-    printf("rrb\n");
+    //printf("rrb\n");
+    put_command_in_list(RRB,command_list);
     node_t* tail = pop_tail(list_b);
     if(!tail) return;
     push_head(list_b, tail);
 }
 //rrr : rra and rrb at the same time
-void rrr(list_t* list_a, list_t *list_b, int print){
+void rrr(list_t* list_a, list_t *list_b, int print,list_t *command_list){
     if (print)
-    printf("rrr\n");
-
-    rra(list_a, 0);
-    rrb(list_b, 0);
+    //printf("rrr\n");
+    put_command_in_list(RRR,command_list);
+    rra(list_a, 0,command_list);
+    rrb(list_b, 0,command_list);
 }
--- push_swap.h
+++ push_swap.h
@@ -32,35 +32,52 @@ void	check_input_numbers_are_integers(int argc, char *argv[]);
 void	check_input_numbers_are_not_overflow(int argc, char *argv[]);
 void	check_input_numbers_are_not_underflow(int argc, char *argv[]);
 
-void    sa(list_t* list_a, int print);
-void    sb(list_t* list_b, int print);
-void    ss(list_t* list_a,list_t* list_b, int print);
-void    pa(list_t* list_a, list_t* list_b, int print);
-void    pb(list_t* list_a, list_t* list_b, int print);
-void    ra(list_t* list_a, int print);
-void    rb(list_t* list_a, int print);
-void    r(list_t* list_a, list_t *list_b, int print);
-void    rra(list_t* list_a, int print);
-void    rrb(list_t* list_b, int print);
-void    rrr(list_t* list_a, list_t *list_b, int print); 
+void    sa(list_t* list_a, int print,list_t *command_list);
+void    sb(list_t* list_b, int print,list_t *command_list);
+void    ss(list_t* list_a,list_t* list_b, int print,list_t *command_list);
+void    pa(list_t* list_a, list_t* list_b, int print,list_t *command_list);
+void    pb(list_t* list_a, list_t* list_b, int print,list_t *command_list);
+void    ra(list_t* list_a, int print,list_t *command_list);
+void    rb(list_t* list_a, int print,list_t *command_list);
+void    r(list_t* list_a, list_t *list_b, int print,list_t *command_list);
+void    rra(list_t* list_a, int print,list_t *command_list);
+void    rrb(list_t* list_b, int print,list_t *command_list);
+void    rrr(list_t* list_a, list_t *list_b, int print,list_t *command_list); 
    
 node_t* pop_head(list_t* list);
 node_t* pop_tail(list_t*list);
 void    push_head(list_t* list, node_t* node);
 void    push_tail(list_t* list, node_t* node);
 
-void sortlist_a(list_t * list_a, list_t * list_b, int depth);
-void sortlist_b(list_t * list_b, list_t * list_a, int depth);
+void sortlist_a(list_t * list_a, list_t * list_b, int depth,list_t *command_list);
+void sortlist_b(list_t * list_b, list_t * list_a, int depth,list_t *command_list);
 int get_median(list_t * list, int depth);
-int push_big_toa(list_t*list_a, list_t* list_b, int median, int depth);
-int push_small_tob(list_t*list_a, list_t* list_b, int median, int depth);
-void smallsort_a(list_t *list_a, int depth);
-void smallsort_b(list_t *list_b, int depth);
-void rra_n(list_t* list_a, int n);
-void rrb_n(list_t* list_b, int n);
-void pa_n(list_t * list_a, list_t * list_b, int n);
-void pb_n(list_t * list_b, list_t * list_a, int n);
+int push_big_toa(list_t*list_a, list_t* list_b, int median, int depth,list_t *command_list);
+int push_small_tob(list_t*list_a, list_t* list_b, int median, int depth,list_t *command_list);
+void smallsort_a(list_t *list_a, int depth,list_t *command_list);
+void smallsort_b(list_t *list_b, int depth,list_t *command_list);
+void rra_n(list_t* list_a, int n,list_t *command_list);
+void rrb_n(list_t* list_b, int n,list_t *command_list);
+void pa_n(list_t * list_a, list_t * list_b, int n,list_t *command_list);
+void pb_n(list_t * list_b, list_t * list_a, int n,list_t *command_list);
 
 void print_list( list_t * list);
+void put_command_in_list(int command, list_t * command_list);
+void optimize_commands(list_t *command_list, list_t* optimized_list);
+int check_command_replace(int a, int b);
+void put_command_in_list(int command, list_t *command_list);
+
+#define SA 1
+#define SB 2
+#define SS 3
+#define PA 4
+#define PB 5
+#define RA 6
+#define RB 7
+#define RR 8
+#define RRA 9
+#define RRB 10
+#define RRR 11
+
 
 #endif
